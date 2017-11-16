28a37797a04aa319707d1acd61890b276d0a2c6e
ShihaoXu <konto.shxu@gmail.com>
2017-11-16 04:12:21 +0100

    install and use zweilight theme

---
+++ elpa/zweilight-theme-20170112.2205/zweilight-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "zweilight-theme" "20170112.2205" "A dark color theme for Emacs." 'nil :commit "7f45ab9e23164d65538edb2beb9692ecdc24c31e" :url "http://github.com/philiparvidsson/zweilight-emacs")
--- init.el
+++ init.el
@@ -7,3 +7,19 @@
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize) ;; You might already have this line
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (zweilight-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;;  ----------  begin: my customization  ----------
+(load-theme 'zweilight t)
+;;  ----------  end:   my customization  ----------
