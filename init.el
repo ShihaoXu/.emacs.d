d958c9acfabf7e2b2c12cb7f3eff048f210a3f2c
ShihaoXu <konto.shxu@gmail.com>
2017-11-16 19:48:08 +0100

    dakrone theme in use

--- init.el
+++ init.el
@@ -12,7 +12,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (dired-subtree dired+ zweilight-theme))))
+ '(package-selected-packages
+   (quote
+    (dakrone-theme dired-subtree dired+ zweilight-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -21,5 +23,5 @@
  )
 
 ;;  ----------  begin: my customization  ----------
-(load-theme 'zweilight t)
+(load-theme 'dakrone t)
 ;;  ----------  end:   my customization  ----------
